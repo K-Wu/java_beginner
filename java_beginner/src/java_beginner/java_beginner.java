0374e6c7ab4c60d8709e5ce2fd8ae23d6b529025
K. Wu <tonywukun@qq.com>
2015-10-03 10:01:47 +0800

    helloworld

---
+++ java_beginner/src/java_beginner/java_beginner.java
@@ -0,0 +1,10 @@
+package java_beginner;
+
+public class java_beginner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
